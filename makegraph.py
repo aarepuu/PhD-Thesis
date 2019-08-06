3e77d8173763816d84523833cce9232a9d704ca9
Aare Puussaar <aare.puussaar@gmail.com>
2019-08-06 19:33:36 +0100

    fixed progress graph dates

--- makegraph.py
+++ makegraph.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python3
 
+import datetime
 import sys
-import os
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 
+# ticks formaters
+years = mdates.YearLocator()   # every year
+months = mdates.MonthLocator()  # every month
+weeks =  mdates.WeekdayLocator() # every week
+monthFmt = mdates.DateFormatter('%d-%m-%Y')
 
+# read data
 data = pd.read_csv(sys.stdin,sep=' ', header=0)
+data['date'] = pd.to_datetime(data['date'])
 
+# the plot
+plt.figure()
 plt.plot(data['date'], data['words'], color='r', label='PhD Progress')
 plt.xlabel('Date')
 plt.ylabel('Word Count')
 plt.title('Phd Progress')
+
+# format the ticks
+ax = plt.gca()
+ax.xaxis.set_major_locator(months)
+ax.xaxis.set_major_formatter(monthFmt)
+plt.gcf().autofmt_xdate() # Rotation
 plt.savefig('word_count.png')
